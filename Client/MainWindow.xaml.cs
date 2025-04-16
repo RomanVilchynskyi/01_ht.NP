be9869d39fc6040b21d7c1447954bed8269dedc2
Roman <VXlch_vg85@student.itstep.org>
2025-04-16 17:17:44 +0300

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private static string serverAddress = "127.0.0.1"; 
+    private static int serverPort = 4040;  
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        string postalCode = txtZip.Text;  
+        if (string.IsNullOrEmpty(postalCode))
+        {
+            MessageBox.Show("Please enter a postal code");
+            return;
+        }
+
+        string result = await GetStreetsByPostalCode(postalCode);
+
+        txtResult.Text = result;
+    }
+
+    private async Task<string> GetStreetsByPostalCode(string postalCode)
+    {
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(serverAddress), serverPort);
+        EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+        try
+        {
+            byte[] sendBytes = Encoding.UTF8.GetBytes(postalCode);
+            socket.SendTo(sendBytes, serverEndPoint);
+
+            byte[] receivedData = new byte[1024];
+            int bytesReceived = socket.ReceiveFrom(receivedData, ref remoteEndPoint);
+
+            string result = Encoding.UTF8.GetString(receivedData, 0, bytesReceived);
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+
+            return ex.Message;
+        }
+        finally
+        {
+            socket.Close(); 
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
+
+internal class Program
+{
+    static string address = "127.0.0.1";
+    static int port = 4040;
+
+    private static string GetStreetsByPostalCode(string postalCode)
+    {
+        try
+        {
+            string[] lines = File.ReadAllLines("streets.txt");
+            var streets = new StringBuilder();
+            bool firstStreet = true;
+
+            foreach (var line in lines)
+            {
+                string[] parts = line.Split(' ');
+                if (parts[0] == postalCode)
+                {
+                    if (!firstStreet)
+                    {
+                        streets.Append(", ");
+                    }
+
+                    for (int i = 1; i < parts.Length; i++)
+                    {
+                        streets.Append(parts[i] + " ");
+                    }
+
+                    firstStreet = false;
+                }
+            }
+
+            if (streets.Length > 0)
+            {
+                return streets.ToString();
+            }
+            else
+            {
+                return "No streets found for this postal code";
+            }
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+    }
+    private static void Main(string[] args)
+    {
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(address), port);
+        EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        try
+        {
+            listenSocket.Bind(endPoint);
+            Console.WriteLine("Server started! Waiting for connection");
+
+            while (true)
+            {
+                byte[] data = new byte[1024];
+                listenSocket.ReceiveFrom(data, ref remoteEndPoint);
+
+                string postalCode = Encoding.Unicode.GetString(data);
+                Console.WriteLine($"{DateTime.Now.ToShortTimeString()} :: Received postal code: {postalCode} from {remoteEndPoint}");
+
+                string streets = GetStreetsByPostalCode(postalCode);
+
+                data = Encoding.Unicode.GetBytes(streets);
+                listenSocket.SendTo(data, remoteEndPoint);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            listenSocket.Shutdown(SocketShutdown.Both);
+            listenSocket.Close();
+        }
+    }
+
+}
