2e50ee327d3834f1200b9549fc9f9aad6802059b
Roman <VXlch_vg85@student.itstep.org>
2025-05-04 22:18:39 +0300

    ---

---
+++ 04_ht_Server/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+internal class Program
+{
+    static TcpListener listener;
+    static List<StreamWriter> sw = new List<StreamWriter>();
+
+    static void Broadcast(string message, StreamWriter exclude)
+    {
+        lock (sw)
+        {
+            foreach (var writer in sw)
+            {
+                if (writer != exclude)
+                {
+                    try
+                    {
+                        writer.WriteLine(message);
+                        writer.Flush();
+                    }
+                    catch { }
+                }
+            }
+        }
+    }
+
+    static void ClientAdd(TcpClient client)
+    {
+        var stream = client.GetStream();
+        StreamReader reader = new StreamReader(stream);
+        StreamWriter writer = new StreamWriter(stream);
+
+        lock (sw) sw.Add(writer);
+
+        try
+        {
+            while (true)
+            {
+                string message = reader.ReadLine()!;
+                if (message == null) break;
+
+                Console.WriteLine("recieved: " + message);
+                Broadcast(message, writer);
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Client left");
+        }
+        finally
+        {
+            lock (sw) sw.Remove(writer);
+            client.Close();
+        }
+    }
+    static int MAX_CLIENTS = 5;
+    static void Main()
+    {
+        listener = new TcpListener(IPAddress.Any, 8080);
+        listener.Start();
+        Console.WriteLine("Server started...");
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+
+            lock (sw)
+            {
+                if (sw.Count >= MAX_CLIENTS)
+                {
+                    var stream = client.GetStream();
+                    StreamWriter tmp = new StreamWriter(stream);
+                    tmp.WriteLine("Try later");
+                    tmp.Flush();
+                    tmp.Close();
+                    client.Close();
+                    continue;
+                }
+            }
+
+            Console.WriteLine("New client added");
+            Thread thread = new Thread(() => ClientAdd(client));
+            thread.Start();
+        }
+    }
+}
---
+++ 05_ht_smtp/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Win32;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _05_ht_smtp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private List<string> attachments = new List<string>();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void SelectFile(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog dialog = new OpenFileDialog();
+        dialog.Multiselect = true;
+
+        if (dialog.ShowDialog() == true)
+        {
+            foreach (string file in dialog.FileNames)
+            {
+                attachments.Add(file);
+                listFiles.Items.Add(System.IO.Path.GetFileName(file));
+            }
+        }
+    }
+
+    private void SendMessage(object sender, RoutedEventArgs e)
+    {
+        string email = emailBox.Text;
+        string password = passwordBox.Password;
+        string to = toBox.Text;
+        string subject = themeBox.Text;
+        string body = new TextRange(messageBox.Document.ContentStart, messageBox.Document.ContentEnd).Text;
+
+        if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password) ||
+            string.IsNullOrWhiteSpace(to))
+        {
+            MessageBox.Show("Email, password and address are obilged");
+            return;
+        }
+
+        try
+        {
+            MailMessage message = new MailMessage(email, to, subject, body)
+            {
+                IsBodyHtml = false,
+                Priority = MailPriority.Normal
+            };
+
+            foreach (string file in attachments)
+                message.Attachments.Add(new Attachment(file));
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587) 
+            {
+                Credentials = new NetworkCredential(email, password),
+                EnableSsl = true
+            };
+
+            client.Send(message);
+            MessageBox.Show("Message sent successfuly");
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+}
